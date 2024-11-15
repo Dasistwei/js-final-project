8442f7dc2a1f61330a1079fb0901b47983053e7d
wei <wei02919@gmail.com>
2024-11-15 21:20:50 +0800

    add admin.html & client.html

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/js-final-project/' : '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        admin: './admin.html',
+      },
+    },
+  },
 })
